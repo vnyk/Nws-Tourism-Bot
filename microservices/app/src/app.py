03a3aaeb9595faa7e8bcfdc00cf3c388cfebf54a
vnyk <uservks@gmail.com>
2018-01-13 00:28:40 +0530

    My first commitdfssdse

---
+++ microservices/app/src/app.py
@@ -0,0 +1,104 @@
+import os, sys
+from flask import Flask, request
+from pymessenger import Bot
+from utils import wit_response, get_news_elements
+
+app = Flask(__name__)
+accesstoken="EAAFtFYZBO3ZBwBAOhkocvvI5QiXiSquvuHjrVdvw9n0CgdQHkR5sHoX3V39GoMMYqIopY1RaXGV917aECavKFmUyXNnQUplW16n6MFgFsu7s8BgQu5qCvpLzEQXoZBvjs1wHHFv8EZCZC44XKN1spbjdT1TZAhnKeiVNLoLJSMVgZDZD"
+bot=Bot(accesstoken)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+	# Webhook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+	data = request.get_json()
+	log(data)
+
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
+
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'no text'
+
+					# Echo
+					response = None
+					#categories=None
+					entity, value, categories=wit_response(messaging_text)
+
+					if entity =='location':
+						response="Welcome to {}".format(str(value))
+						categories="{}".format(str(value))
+
+					if entity=='developer':
+						elements=[]
+						element = {
+												'title': "Vinayak Sharma",
+												'buttons': [{
+															'type': 'web_url',
+															'title': "LinkedIn",
+															'url': "https://www.linkedin.com/in/vinayak-sharma-2ab488142"
+												},
+												{
+															'type': 'web_url',
+															'title': "Website",
+															'url': "http://vinayaksharma.blog"
+												},
+												{
+															'type': 'web_url',
+															'title': "Facebook",
+															'url': "https://www.facebook.com/srma.vnyk19"
+												}],
+												'image_url': "https://spiderimg.amarujala.com/assets/images/2017/12/03/500x500/udaipur_1512275413.jpeg"
+									}
+						elements.append(element)
+						bot.send_generic_message(sender_id, elements)
+						response="Vinayak made me <3"
+
+
+					if entity=='greetings':
+						cresponse="Hi! I am Bhaya from Rajasthan"
+						'''Cus_response="Hi! I am Bhaya from Rajasthan"'''
+						response="how can I help you?"
+						bot.send_text_message(sender_id, cresponse)
+
+					if entity =='news':
+						if(categories):
+							elements=get_news_elements(categories)
+							bot.send_generic_message(sender_id, elements)
+							response="here it is! what else?"
+						else:
+							response="Where are u in Rajastan?"
+						
+					if response==None:
+						response="please speak clearly!"
+
+					bot.send_text_message(sender_id, response)
+
+	return "ok", 200
+
+
+def log(message):
+	print(message)
+	sys.stdout.flush()
+
+
+if __name__ == "__main__":
+	app.run(debug = True, port = 80)
---
+++ microservices/app/src/utils.py
@@ -0,0 +1,54 @@
+from wit import Wit
+from gnewsclient import gnewsclient
+
+witaccess_token="GFDAPYSJK5XBBUGOJYPHWDQI4LGJWSIQ"
+
+client=Wit(access_token= witaccess_token)
+
+loc={'area':None}
+def wit_response(message_text):
+	resp=client.message(message_text)
+	
+	entity=None
+	value=None
+	categories=None
+	try:
+		entity=list(resp['entities'])[0]
+		
+		value=resp['entities'][entity][0]['value']
+		if(entity=='location'):
+			loc['area']=None
+			loc['area']="{}".format(str(value))
+		categories={'news':None}
+		categories[entity] = resp['entities'][entity][0]['value']
+	except:
+		pass
+
+	return(entity,value,categories)
+
+
+def get_news_elements(categories):
+	news_client = gnewsclient()
+	news_client.query = ''
+	news_client.location=loc['area']
+
+	if categories['news'] != None:
+		news_client.query += categories['news'] + ' '
+
+	news_items = news_client.get_news()
+
+	elements = []
+
+	for item in news_items:
+		element = {
+					'title': item['title'],
+					'buttons': [{
+								'type': 'web_url',
+								'title': "Read more",
+								'url': item['link']
+					}],
+					'image_url': item['img']		
+		}
+		elements.append(element)
+
+	return elements
